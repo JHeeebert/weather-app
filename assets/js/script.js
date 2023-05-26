94c774a30a4a9bcd6f56505bdbda828ed88b4ac8
Joshua Hebert <jhebertcocktails@gmail.com>
2023-05-26 01:02:50 -0400

    JS NOTES ADDED HTML TIGHTENED UP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 //Universal Variables
-var searchHistory = []
-var city = ""
-var apiUrl = 'https://fcc-weather-api.glitch.me/api/current?lon=-80.7108608' + '&lat=35.2387072';
+var searchHistory = ('#search-history-list');
+var searchCityInput = ("#search-city");
+var searchCityButton= ("#search-city-button");
+var clearHistoryButton = ("#clear-history");
+var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lon=-80.7108608' + '&lat=35.2387072&appid=10dd70968c138de58593c18c06b36159';
 //Call to OpenWeather API
 fetch(apiUrl)
   .then(response => {
@@ -10,15 +12,16 @@ fetch(apiUrl)
     }
   })
   .then(data => {
-    //Current City Weather Data based off Lat/Lon coordinates from openWeather API 
-    //that we can use for generating information
+    //Pulls Current City Weather Data based off Lat/Lon coordinates from openWeather API 
+    console.log(data);
     //Current Weather Forecast
     var currDate = document.getElementsByClassName('current-date')[0];
     var currIcon = document.getElementsByClassName('current-icon')[0];
     var currTemp = document.getElementsByClassName('current-temp')[0];
     var currWind = document.getElementsByClassName('current-wind')[0];
     var currHumid = document.getElementsByClassName('current-humid')[0];
-    console.log(data);
+   //function writeCurrent CREATE FUNcTION TO WRITE CURRENT_FORECAST TO CURRENT_CONTENT
+
   })
 //City Search Bar
 const submitForm = function (event) {
@@ -32,7 +35,7 @@ const submitForm = function (event) {
     alert("You Must Enter A City Name")
   }
 }
-var fiverUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=New%20York&appid=10dd70968c138de58593c18c06b36159&cnt=5';
+var fiverUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Charlotte&appid=10dd70968c138de58593c18c06b36159&cnt=5';
 fetch(fiverUrl)
   .then(response => {
     if (response.ok) {
@@ -40,14 +43,15 @@ fetch(fiverUrl)
     }
   })
   .then(fiverData => {
-    //Five Day Forecast Data from API That we can use for generating information
+    console.log(fiverData);
+    //Pulls Five-Day-Forecast Data for Charlotte NC from OpenWeather API
     //Five Day Forecast
     var fiverDate = document.getElementsByClassName('fiver-date')[0];
     var fiverIcon = document.getElementsByClassName('fiver-icon')[0];
     var fiverTemp = document.getElementsByClassName('fiver-temp')[0];
     var fiverWind = document.getElementsByClassName('fiver-wind')[0];
     var fiverHumid = document.getElementsByClassName('fiver-humid')[0];
-    console.log(fiverData);
+  //function writeFiver CREATE FUNcTION TO WRITE FIVE_DAY_FORECAST TO FIVER_CONTENT
   })
 
 
